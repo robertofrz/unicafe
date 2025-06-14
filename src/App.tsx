897b666d681a94d7d902c57f8b501b91661dc553
Roberto Ferraz <robertofrz94@gmail.com>
2025-06-14 12:06:11 -0300

    bg image and font updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import FeedbackButtons from "./components/FeedbackContainer";
 function App() {
   return (
     <div className="flex flex-col items-center justify-between gap-2 sm:w-[400px] w-[90vw] sm:h-[600px] h-[70vh] bg-[#e9d8c7] rounded-lg p-3 border-t-6 border-b-6 border-[#82512D] shadow-lg text-center text-[#6e4527]">
-      <h1 className="expresso text-6xl -mb-3">Unicafe</h1>
-      <p className="inter opacity-70 font-bold mb-2">
+      <h1 className="coffee text-5xl -mb-2">Unicafe o</h1>
+      <p className="inter opacity-70 font-bold mb-1">
         Help us serve you better! Give your feedback about our coffee shop.
       </p>
       <FeedbackButtons />
